906c3227e5eb71489b27591a63c71a3083c0c2a7
ឡៃហ៊ីម 来兴 LayhimXC​ <layhimxc@layhimxc-PORTEGE-M900.(none)>
2013-07-30 08:52:12 +0700

    layhim commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppRubbyOnRails::Application.config.secret_token = 'bb56bf3d0a1eb82e0ceb2f2ddc3f685d67c6aaee043b57f0b2b0d4871093bea9e9558b403385c6443249339f9d9b2c12e34042c641a2eed1afabe0b5b9d0fd55'
